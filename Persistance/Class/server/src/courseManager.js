8c439ed03d9991174cd8a44c81a380e2572fcc4e
Selamet53 <selamet.zamanoglu@polymtl.ca>
2023-11-24 16:42:20 -0500

    update

--- Persistance/Class/server/src/courseManager.js
+++ Persistance/Class/server/src/courseManager.js
@@ -1,71 +1,72 @@
 const fsPromises = require('fs').promises;
 const path = require('path');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const DB_CONSTS = require("./env");
 
 class CourseManager {
     constructor() {
-        this.courses = [];
-        this.path = path.join(__dirname + "../../data/courses.json");
+        // this.courses = [];
+        // this.path = path.join(__dirname + "../../data/courses.json");
+
+        // MongoDB
+        this.collection = null;
+        this.client = null;
+        this.options = {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        };
     }
 
     async init() {
         try {
-            const courses = await fsPromises.readFile(this.path);
-            this.courses = JSON.parse(courses);
+            this.client = await MongoClient.connect(DB_CONSTS.DB_URL, this.options);
+            this.collection = this.client.db(DB_CONSTS.DB_DB).collection(DB_CONSTS.DB_COLLECTION);
+            // Force "sigle" a être unique dans les documents
+            this.collection.createIndex("sigle", { unique: true });
+            console.log("Connexion à la base de données réussie");
         }
         catch (e) {
-            console.log(`Aucun fichier trouvé. Chargement d'une liste vide`);
-            this.courses = [];
+            console.log(`Vous n'avez pas réussit à vous connecter à MongoDB`);
         }
-        finally {
-            return this.courses;
-        }
-    }
-
-    async saveCoursesToFile() {
-        await fsPromises.writeFile(this.path, JSON.stringify(this.courses, null, 4));
     }
 
     async getAllCourses() {
-        return this.courses;
+        return this.collection.find({}, { projection: { _id: 0 } }).toArray();
     }
 
     findCourse(sigle) {
-        return this.courses.find((c) => {
-            return c.sigle === sigle;
-        });
+        return this.collection.findOne({sigle: sigle}, { projection: { _id: 0 } });
     }
 
     async addCourse(sigle) {
         const course = { sigle, credits: Math.floor(Math.random() * 5 + 1) };
-        if (this.courses.find(x => x.sigle === course.sigle)) {
+        try {
+            await this.collection.insertOne(course);
+            return true;
+        }
+        catch (e) {
             return false;
         }
-        this.courses.push(course);
-        await this.saveCoursesToFile();
-        return true;
     }
 
     async deleteCourse(sigle) {
-        const size = this.courses.length;
-        this.courses = this.courses.filter((c) => c.sigle !== sigle);
-        await this.saveCoursesToFile();
-        return size > this.courses.length;
+        const deleteResult = await this.collection.deleteMany({ sigle });
+        return deleteResult.deletedCount;
     }
 
     async modifyCourse(modifiedCourse) {
-        const course = this.courses.find((c) => {
-            return c.sigle === modifiedCourse.sigle;
-        });
-        if (!course) return false;
-        course.credits = modifiedCourse.credits;
-        await this.saveCoursesToFile();
-        return true;
+        const modifiedResult = await this.collection.updateOne(
+            { sigle: modifiedCourse.sigle },
+            { $set: { credits: modifiedCourse.credits } });
+        return modifiedResult.matchedCount;
     }
 
     async deleteAll() {
         try {
-            await fsPromises.unlink(this.path);
-            this.courses = [];
+            await this.collection.deleteMany({});
             return true;
         }
         catch (e) {
