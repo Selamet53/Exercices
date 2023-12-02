725f90459234bbcad85292a214a15117f452565f
Selamet53 <selamet.zamanoglu@polymtl.ca>
2023-12-01 23:34:52 -0500

    Update useStorage.jsx

--- React/Hooks/useStorage/src/useStorage.jsx
+++ React/Hooks/useStorage/src/useStorage.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
 
 export default function useStorage(key, defaultData, isSession = false) {
-    return ['', () => { }];
+    const storage = isSession ? sessionStorage : localStorage;
+    const [data, setData] = useState(
+        JSON.parse(storage.getItem(key)) || String(defaultData)
+    );
+
+    useEffect(() => {
+        storage.setItem(key, JSON.stringify(data));
+    }, [data, key]);
+
+    useEffect(() => {
+        window.addEventListener('storage', (e) => {
+            if (e.key === null) {
+                setData(String(defaultData));
+            }
+        });
+    }, []);
+
+    return [data, setData];
 };
